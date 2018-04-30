281b4ce12e933cfc69078cfbe1c6b6f87f3e440f
valmat <ufabiz@gmail.com>
2018-04-30 15:53:25 +0600

    Added unittests for module vest.json.utils

--- exmpls/json_utils.d
+++ exmpls/json_utils.d
@@ -2,14 +2,9 @@
 
 import std.stdio     : writeln;
 import std.algorithm : map;
-import std.typecons  : tuple, Tuple;
-import std.range     : iota;
-import vest.json     ;
-import std.json     ;
-import std.array : array     ;
-
-
-
+import std.json      : parseJSON;
+import std.array     : array;
+import vest.json     : checkNull, numeric;
 
 void main()
 {
@@ -17,14 +12,10 @@ void main()
     auto js = parseJSON(`[-1,1,0.5,-0.5]`);
     js.toPrettyString.writeln;
 
-    
-
     js.array.map!(x => x.numeric).array.writeln;
 
     parseJSON(`0.5`).numeric.writeln;
     
-    //parseJSON(`"dfg"`).numeric.writeln; // thrown an exception
-
     parseJSON(`{"a":5,"b":null}`).checkNull("a").writeln;
     parseJSON(`{"a":5,"b":null}`).checkNull("b").writeln;
     parseJSON(`{"a":5,"b":null}`).checkNull("c").writeln;
--- source/vest/json/utils.d
+++ source/vest/json/utils.d
@@ -1,10 +1,7 @@
 module vest.json.utils;
 
-import std.json      : JSONValue, JSONException, JSON_TYPE;
-import std.array     : array, empty;
-import std.algorithm : map, filter;
-//import std.traits    : isType, isFunction, isArray, isAssociativeArray, isIterable, isPointer, isSomeChar, isSomeString;
-import std.conv      : to;
+import std.json : JSONValue, JSONException, JSON_TYPE;
+import std.conv : to;
 
 // Cast to any numeric
 double numeric()(auto ref JSONValue v) {
@@ -25,4 +22,19 @@ const(JSONValue)* checkNull()(auto ref JSONValue v, string field) pure @safe
 {
     const(JSONValue)* ptr =  field in v;
     return (ptr && ptr.type != JSON_TYPE.NULL) ? ptr : null;
+}
+
+// cd source
+// rdmd -unittest -main  vest/json/utils
+unittest {
+    import std.algorithm : map;
+    import std.json      : parseJSON;
+    import std.math      : approxEqual;
+
+    assert([-1, 1, 0.5, -0.5].approxEqual(parseJSON(`[-1,1,0.5,-0.5]`).array.map!(x => x.numeric)));
+    assert(parseJSON(`0.5`).numeric.approxEqual(0.5));
+    assert(null != parseJSON(`{"a":5,"b":null}`).checkNull("a"));
+    assert(null == parseJSON(`{"a":5,"b":null}`).checkNull("b"));
+    assert(null == parseJSON(`{"a":5,"b":null}`).checkNull("c"));
+    assert( (*parseJSON(`{"a":5,"b":null}`).checkNull("a")).integer == 5);
 }
